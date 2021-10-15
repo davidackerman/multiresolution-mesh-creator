662eeb3b36ad96eeae3056ff75da70704faa3d9b
David Ackerman <ackermand@janelia.hhmi.org>
2021-10-15 10:29:38 -0400

    Add function docstrings and reformatting

--- createMultiresMeshes.py
+++ createMultiresMeshes.py
@@ -1,103 +1,151 @@
 import trimesh
+from trimesh.intersections import slice_faces_plane
 import numpy as np
 from io_utils import stdout_redirected
 from dvidutils import encode_faces_to_custom_drc_bytes
 import dask
 import utils
 import time
-
-from collections import namedtuple
 import openmesh as om
 import sys
 import os
 import pyfqmr
-from dask.distributed import Client, progress, get_client, worker_client
+from dask.distributed import Client, worker_client
 from utils import Fragment
-import logging
 from numba import jit
 
 
 @jit(nopython=True)
-def get_faces_within_slice(v, f, fv, max_edge_length, plane_normal, plane_origin):
-    ax = np.where(plane_normal != 0)[0][0]
-    plane_origin = plane_origin[ax]
+def get_faces_within_slice(vertices, faces, triangles, max_edge_length,
+                           plane_normal, plane_origin):
+    """Numba optimized function to get mesh faces to the positive normal side
+    of plane_origin with a padding of max_edge_length. A face is defined to be
+    within this region if at least one of its vertices is within the region.
+
+    Args:
+        vertices: Mesh vertices
+        faces: Mesh faces
+        triangle: Array of mesh face vertices
+        max_edge_length: Maximum edge length for mesh
+        plane_normal: Normal of plane
+        plane_origin: Origin of plane
+
+    Returns:
+        vertices, faces: Vertices and faces within region
+    """
+    axis = np.where(plane_normal != 0)[0][0]
+    plane_origin = plane_origin[axis]
     if np.any(plane_normal < 0):
-        plane_origin = plane_origin+max_edge_length
-        faces_in_range = np.where(
-            (fv[:, 0+ax] <= plane_origin) | (fv[:, 3+ax] <= plane_origin) | (fv[:, 6+ax] <= plane_origin))[0]
+        plane_origin = plane_origin + max_edge_length
+        faces_in_range = np.where((triangles[:, 0 + axis] <= plane_origin)
+                                  | (triangles[:, 3 + axis] <= plane_origin)
+                                  | (triangles[:, 6 + axis] <= plane_origin))
     else:
-        plane_origin = plane_origin-max_edge_length
-        faces_in_range = np.where(
-            (fv[:, 0+ax] >= plane_origin) | (fv[:, 3+ax] >= plane_origin) | (fv[:, 6+ax] >= plane_origin))[0]
+        plane_origin = plane_origin - max_edge_length
+        faces_in_range = np.where((triangles[:, 0 + axis] >= plane_origin)
+                                  | (triangles[:, 3 + axis] >= plane_origin)
+                                  | (triangles[:, 6 + axis] >= plane_origin))
 
-    f = f[faces_in_range].reshape(-1)
+    faces_in_range = faces_in_range[0]
+    faces = faces[faces_in_range].reshape(-1)
 
-    v_unq = np.unique(f)  # Numba doesn't support return_inverse argument
-    v_renumbering_dict = {}
-    for idx, v_unq_idx in enumerate(v_unq):
-        v_renumbering_dict[v_unq_idx] = idx
+    vertices_unq = np.unique(
+        faces)  # Numba doesn't support return_inverse argument
+    vertices_renumbering_dict = {}
+    for idx, vertex_unq_idx in enumerate(vertices_unq):
+        vertices_renumbering_dict[vertex_unq_idx] = idx
 
-    f = np.array([v_renumbering_dict[v_idx] for v_idx in f])
+    faces = np.array([vertices_renumbering_dict[v_idx] for v_idx in faces])
 
-    v = v[v_unq]
+    vertices = vertices[vertices_unq]
 
-    f = f.reshape(-1, 3)
-    return v, f
-
-
-def my_fast_slice_faces_plane(v, f, fv, max_edge_length, plane_normal, plane_origin):
-    if len(v) > 0:
-        v, f = get_faces_within_slice(
-            v, f, fv, max_edge_length, plane_normal, plane_origin)
-        v, f = my_slice_faces_plane(
-            v, f, plane_normal, plane_origin)
-    return v, f
+    faces = faces.reshape(-1, 3)
+    return vertices, faces
 
 
-def my_slice_faces_plane(v, f, plane_normal, plane_origin):
-    # Wrapper for trimesh slice_faces_plane, checks that there are vertices and faces and catches an error that happens if the whole mesh is to one side
+def my_fast_slice_faces_plane(vertices, faces, triangles, max_edge_length,
+                              plane_normal, plane_origin):
+    """slice_faces_plane but with numba optimized prestep
+    to get faces within slice. Ideally would speed up code.
 
-    if len(v) > 0 and len(f) > 0:
+    Args:
+        vertices: Mesh vertices
+        faces: Mesh faces
+        triangles: Array of mesh face vertices
+        max_edge_length: Maximum edge length for mesh
+        plane_normal: Normal of plane
+        plane_origin: Origin of plane
+
+    Returns:
+        v, f: Vertices and faces
+    """
+    if len(vertices) > 0:
+        vertices, faces = get_faces_within_slice(vertices, faces, triangles,
+                                                 max_edge_length, plane_normal,
+                                                 plane_origin)
+
+        vertices, faces = my_slice_faces_plane(vertices, faces, plane_normal,
+                                               plane_origin)
+
+    return vertices, faces
+
+
+def my_slice_faces_plane(vertices, faces, plane_normal, plane_origin):
+    """Wrapper for trimesh slice_faces_plane to cath error that happens if the
+    whole mesh is to one side of the plane.
+
+    Args:
+        vertices: Mesh vertices
+        faces: Mesh faces
+        plane_normal: Normal of plane
+        plane_origin: Origin of plane
+
+    Returns:
+        vertices, faces: Vertices and faces
+    """
+    if len(vertices) > 0 and len(faces) > 0:
         try:
-            v, f = trimesh.intersections.slice_faces_plane(
-                v, f, plane_normal=plane_normal, plane_origin=plane_origin)
+            vertices, faces = slice_faces_plane(vertices, faces, plane_normal,
+                                                plane_origin)
         except ValueError as e:
             if str(e) != "input must be 1D integers!":
                 raise
             else:
                 pass
 
-    return v, f
+    return vertices, faces
 
 
-def update_dict(combined_fragments_dictionary, fragment_origin, v, f, lod_0_fragment_position):
-    if fragment_origin in combined_fragments_dictionary:
-        [v_combined, f_combined,
-            lod_0_fragment_positions_combined] = combined_fragments_dictionary[fragment_origin]
+def update_fragment_dict(dictionary, fragment_pos, vertices, faces, lod_0_fragment_pos):
+    if fragment_pos in dictionary:
+        [vertices_combined, faces_combined,
+         lod_0_fragment_pos_combined] = dictionary[fragment_pos]
 
-        f_combined = np.vstack((f_combined, f+len(v_combined)))
-        v_combined = np.vstack((v_combined, v))
-        lod_0_fragment_positions_combined.append(lod_0_fragment_position)
+        faces_combined = np.vstack(
+            (faces_combined, faces + len(vertices_combined)))
+        vertices_combined = np.vstack((vertices_combined, vertices))
+        lod_0_fragment_pos_combined.append(fragment_pos)
 
-        combined_fragments_dictionary[fragment_origin] = [
-            v_combined, f_combined, lod_0_fragment_positions_combined]
+        dictionary[fragment_pos] = [
+            vertices_combined, faces_combined, lod_0_fragment_pos_combined
+        ]
     else:
-        combined_fragments_dictionary[fragment_origin] = [
-            v, f, [lod_0_fragment_position]]
-
-    return combined_fragments_dictionary
+        dictionary[fragment_pos] = [vertices, faces, [lod_0_fragment_pos]]
 
 
 @dask.delayed
-def generate_mesh_decomposition(v, f, lod_0_box_size, start_fragment, end_fragment, x_start,  x_end, current_lod):
+def generate_mesh_decomposition(vertices, faces, lod_0_box_size,
+                                start_fragment, end_fragment, x_start, x_end,
+                                current_lod):
     combined_fragments_dictionary = {}
     fragments = []
 
     nyz, nxz, nxy = np.eye(3)
 
     if current_lod != 0:
-        sub_box_size = lod_0_box_size*2**(current_lod-1)
-        start_fragment *= 2  # since want it to be divisible by 2x2x2 subregions
+        sub_box_size = lod_0_box_size * 2**(current_lod - 1)
+        # Want each chunk for lod>0 to be divisible by 2x2x2 region, so multiply by 2
+        start_fragment *= 2
         end_fragment *= 2
         x_start *= 2
         x_end *= 2
@@ -105,16 +153,19 @@ def generate_mesh_decomposition(v, f, lod_0_box_size, start_fragment, end_fragme
         sub_box_size = lod_0_box_size
 
     for x in range(x_start, x_end):
-        vx, fx = my_slice_faces_plane(
-            v, f, plane_normal=-nyz, plane_origin=nyz*(x+1)*sub_box_size)
+        plane_origin_yz = nyz * (x + 1) * sub_box_size
+        vertices_yz, faces_yz = my_slice_faces_plane(vertices, faces, -nyz,
+                                                     plane_origin_yz)
 
         for y in range(start_fragment[1], end_fragment[1]):
-            vy, fy = my_slice_faces_plane(
-                vx, fx, plane_normal=-nxz, plane_origin=nxz*(y+1)*sub_box_size)
+            plane_origin_xz = nxz * (y + 1) * sub_box_size
+            vertices_xz, faces_xz = my_slice_faces_plane(
+                vertices_yz, faces_yz, -nxz, plane_origin_xz)
 
             for z in range(start_fragment[2], end_fragment[2]):
-                vz, fz = my_slice_faces_plane(
-                    vy, fy, plane_normal=-nxy, plane_origin=nxy*(z+1)*sub_box_size)
+                plane_origin_xy = nxy * (z + 1) * sub_box_size
+                vertices_xy, faces_xy = my_slice_faces_plane(
+                    vertices_xz, faces_xz, -nxy, plane_origin_xy)
 
                 lod_0_fragment_position = tuple(np.asarray([x, y, z]))
                 if current_lod != 0:
@@ -122,27 +173,35 @@ def generate_mesh_decomposition(v, f, lod_0_box_size, start_fragment, end_fragme
                 else:
                     fragment_position = lod_0_fragment_position
 
-                combined_fragments_dictionary = update_dict(
-                    combined_fragments_dictionary, fragment_position, vz, fz, list(lod_0_fragment_position))
+                update_fragment_dict(
+                    combined_fragments_dictionary, fragment_position,
+                    vertices_xy, faces_xy, list(lod_0_fragment_position))
 
-                vy, fy = my_slice_faces_plane(
-                    vy, fy, plane_normal=nxy, plane_origin=nxy*(z+1)*sub_box_size)
+                vertices_xz, faces_xz = my_slice_faces_plane(
+                    vertices_xz, faces_xz, nxy, plane_origin_xy)
 
-            vx, fx = my_slice_faces_plane(
-                vx, fx, plane_normal=nxz, plane_origin=nxz*(y+1)*sub_box_size)
+            vertices_yz, faces_yz = my_slice_faces_plane(
+                vertices_yz, faces_yz, nxz, plane_origin_xz)
 
-        v, f = my_slice_faces_plane(
-            v, f, plane_normal=nyz, plane_origin=nyz*(x+1)*sub_box_size)
+        vertices, faces = my_slice_faces_plane(vertices, faces, nyz,
+                                               plane_origin_yz)
 
     # return combined_fragments_dictionary
-    for fragment_origin, [v, f, lod_0_fragment_positions] in combined_fragments_dictionary.items():
-        current_box_size = lod_0_box_size*2**current_lod
+    for fragment_origin, [vertices, faces, lod_0_fragment_positions
+                          ] in combined_fragments_dictionary.items():
+        current_box_size = lod_0_box_size * 2**current_lod
         draco_bytes = encode_faces_to_custom_drc_bytes(
-            v, np.zeros(np.shape(v)), f, np.asarray(3*[current_box_size]), np.asarray(fragment_origin)*current_box_size, position_quantization_bits=10)
+            vertices,
+            np.zeros(np.shape(vertices)),
+            faces,
+            np.asarray(3 * [current_box_size]),
+            np.asarray(fragment_origin) * current_box_size,
+            position_quantization_bits=10)
 
         if len(draco_bytes) > 12:
-            fragment = Fragment(draco_bytes, np.asarray(
-                fragment_origin), len(draco_bytes), np.asarray(lod_0_fragment_positions))
+            fragment = Fragment(draco_bytes, np.asarray(fragment_origin),
+                                len(draco_bytes),
+                                np.asarray(lod_0_fragment_positions))
             fragments.append(fragment)
 
     return fragments
@@ -169,7 +228,8 @@ def decimate(v, f, fraction):
         d.initialize()
 
         print(
-            f"Attempting to decimate to {target} (Reduce by {len(v) - target})")
+            f"Attempting to decimate to {target} (Reduce by {len(v) - target})"
+        )
         eliminated_count = d.decimate_to(target)
         print(f"Reduced by {eliminated_count}")
         m.garbage_collection()
@@ -183,12 +243,14 @@ def decimate(v, f, fraction):
 @dask.delayed
 def pyfqmr_decimate(input_path, output_path, id, lod, ext):
     mesh = trimesh.load(f"{input_path}/{id}.{ext}")
-    desired_faces = max(len(mesh.faces)//(2**lod),  1)
+    desired_faces = max(len(mesh.faces) // (2**lod), 1)
 
     mesh_simplifier = pyfqmr.Simplify()
     mesh_simplifier.setMesh(mesh.vertices, mesh.faces)
-    mesh_simplifier.simplify_mesh(
-        target_count=desired_faces, aggressiveness=7, preserve_border=False, verbose=False)
+    mesh_simplifier.simplify_mesh(target_count=desired_faces,
+                                  aggressiveness=7,
+                                  preserve_border=False,
+                                  verbose=False)
     v, f, _ = mesh_simplifier.getMesh()
 
     mesh = trimesh.Trimesh(v, f)
@@ -205,23 +267,27 @@ def generate_multiscale_meshes(input_path, output_path, lods, ids, ext):
         else:
             os.makedirs(f"{output_path}/s{current_lod}", exist_ok=True)
             for id in ids:
-                results.append(pyfqmr_decimate(
-                    input_path, output_path, id, current_lod, ext))
+                results.append(
+                    pyfqmr_decimate(input_path, output_path, id, current_lod,
+                                    ext))
 
     dask.compute(*results)
 
 
-def generate_all_neuroglancer_meshes(output_path, num_workers, ids, lods, original_ext):
+def generate_all_neuroglancer_meshes(output_path, num_workers, ids, lods,
+                                     original_ext):
     results = []
     for id in ids:
-        results.append(generate_single_neuroglancer_mesh(
-            output_path, num_workers, id, lods, original_ext))
+        results.append(
+            generate_single_neuroglancer_mesh(output_path, num_workers, id,
+                                              lods, original_ext))
 
     dask.compute(*results)
 
 
 @dask.delayed
-def generate_single_neuroglancer_mesh(output_path, num_workers, id, lods, original_ext):
+def generate_single_neuroglancer_mesh(output_path, num_workers, id, lods,
+                                      original_ext):
 
     with worker_client() as client:
         os.system(f"rm -rf {output_path}/{id} {output_path}/{id}.index")
@@ -230,7 +296,7 @@ def generate_single_neuroglancer_mesh(output_path, num_workers, id, lods, origin
 
         mesh = []
 
-        lod_0_box_size = 64*4
+        lod_0_box_size = 64 * 4
         results = []
 
         for idx, current_lod in enumerate(lods):
@@ -242,35 +308,50 @@ def generate_single_neuroglancer_mesh(output_path, num_workers, id, lods, origin
 
             v = mesh.vertices
             f = mesh.faces
-            max_edge_length = mesh.edges_unique_length.max()
 
             current_box_size = lod_0_box_size * 2**current_lod
             start_fragment = np.maximum(
-                v.min(axis=0) // current_box_size - 1, np.array([0, 0, 0])).astype(int)
+                v.min(axis=0) // current_box_size - 1,
+                np.array([0, 0, 0])).astype(int)
             end_fragment = (v.max(axis=0) // current_box_size + 1).astype(int)
             x_stride = int(
-                np.ceil(1.0*(end_fragment[0]-start_fragment[0])/num_workers))
+                np.ceil(1.0 * (end_fragment[0] - start_fragment[0]) /
+                        num_workers))
 
-            for x in range(start_fragment[0], end_fragment[0]+1, x_stride):
-                #fv = v.view(np.ndarray)[f]
+            for x in range(start_fragment[0], end_fragment[0] + 1, x_stride):
                 vx, fx = my_slice_faces_plane(
-                    v, f, plane_normal=-nyz, plane_origin=nyz*(x+x_stride)*current_box_size)
+                    v,
+                    f,
+                    plane_normal=-nyz,
+                    plane_origin=nyz * (x + x_stride) * current_box_size)
 
                 if len(vx) > 0:
-                    results.append(generate_mesh_decomposition(
-                        client.scatter(vx), client.scatter(fx), lod_0_box_size, start_fragment, end_fragment, x, x+x_stride, current_lod))
+                    results.append(
+                        generate_mesh_decomposition(client.scatter(vx),
+                                                    client.scatter(fx),
+                                                    lod_0_box_size,
+                                                    start_fragment,
+                                                    end_fragment, x,
+                                                    x + x_stride, current_lod))
 
                     v, f = my_slice_faces_plane(
-                        v, f, plane_normal=nyz, plane_origin=nyz*(x+x_stride)*current_box_size)
+                        v,
+                        f,
+                        plane_normal=nyz,
+                        plane_origin=nyz * (x + x_stride) * current_box_size)
 
-            dask_results = dask.compute(* results)
+            dask_results = dask.compute(*results)
 
             fragments = [
-                fragment for fragments in dask_results for fragment in fragments]
+                fragment for fragments in dask_results
+                for fragment in fragments
+            ]
+
             results = []
             dask_results = []
-            utils.write_mesh_files(output_path, f"{id}", fragments, current_lod, lods[:idx+1], np.asarray(
-                [lod_0_box_size, lod_0_box_size, lod_0_box_size]))
+            utils.write_mesh_files(
+                output_path, f"{id}", fragments, current_lod, lods[:idx + 1],
+                np.asarray([lod_0_box_size, lod_0_box_size, lod_0_box_size]))
 
     print(f"Completed id {id}!")
 
@@ -287,21 +368,21 @@ if __name__ == "__main__":
 
     #
     num_lods = 6
-    input_path = "/groups/cosem/cosem/ackermand/meshesForWebsite/res1decimation0p1/jrc_hela-1/er_seg/"
-    output_path = "/groups/cosem/cosem/ackermand/meshesForWebsite/res1decimation0p1/jrc_hela-1/test_simpler_multires/"
+    input_path = "/groups/cosem/cosem/ackermand/meshesForWebsite/res1decimation0p05/jrc_hela-1/mito_seg/"
+    output_path = "/groups/cosem/cosem/ackermand/meshesForWebsite/res1decimation0p05/jrc_hela-1/test_simpler_multires/mito_seg/"
     results = []
-    ids = list(range(1,  158))
+    ids = list(range(1, 800))
     lods = list(range(num_lods))
 
     t0 = time.time()
     generate_all_neuroglancer_meshes(
         output_path, num_workers, ids, lods, "obj")
-    print(time.time()-t0)
-    # dask_results = dask.compute(*results)
-    # utils.write_segment_properties_file(output_path)
-    # utils.write_info_file(output_path)
+    #print(time.time()-t0)
+    dask_results = dask.compute(*results)
+    utils.write_segment_properties_file(output_path)
+    utils.write_info_file(output_path)
 
     # generate_multiscale_meshes(
-    #     input_path, output_path, lods, ids, 'obj')
+    #      input_path, output_path, lods, ids, 'obj')
 
-    print("end", time.time()-t0)
+    print("end", time.time() - t0)
--- io_utils.py
+++ io_utils.py
@@ -8,22 +8,24 @@ from contextlib import contextmanager
 import ctypes
 from ctypes.util import find_library
 try:
-    libc = ctypes.cdll.msvcrt # Windows
+    libc = ctypes.cdll.msvcrt  # Windows
 except OSError:
     libc = ctypes.cdll.LoadLibrary(find_library('c'))
+
 
 class AutoDeleteDir:
     """
     Stores a directory path and delete that
     directory once this object goes out of scope,
     using __del__(). CPython only.
-    """ 
+    """
+
     def __init__(self, dirpath=None):
         if dirpath is None:
             dirpath = tempfile.mkdtemp()
         self.dirpath = dirpath
         self.skip_delete = False
-    
+
     def __del__(self):
         if not self.skip_delete:
             rmtree(self.dirpath)
@@ -36,17 +38,15 @@ class TemporaryNamedPipe:
     """
     Represents a unix 'named pipe', a.k.a. fifo
     The pipe is created in a temporary directory and deleted upon cleanup() (or __exit__).
-
     Example:
-
         with TemporaryNamedPipe() as pipe:
             def write_hello():
                 with pipe.open_stream('r') as f:
                     f.write("Hello")
             threading.Thread(target=write_hello).start()
-
             subprocess.call(f'cat {pipe.path}')
     """
+
     def __init__(self, basename='temporary-pipe.bin'):
         self.state = 'uninitialized'
         assert '/' not in basename
@@ -88,6 +88,7 @@ class TemporaryNamedPipe:
         An open stream to a parent TemporaryNamedPipe.
         Retains a reference to the parent so the pipe isn't deleted before stream is closed.
         """
+
         def __init__(self, mode, parent_pipe):
             self._parent_pipe = parent_pipe
             self._file = open(parent_pipe.path, mode)
@@ -113,20 +114,37 @@ class TemporaryNamedPipe:
         def __iter__(self):
             return self._file.__iter__()
 
-        def fileno(self, *args, **kwargs): return self._file.fileno(*args, **kwargs)
+        def fileno(self, *args, **
+                   kwargs): return self._file.fileno(*args, **kwargs)
+
         def flush(self, *args, **kwargs): return self._file.flush(*args, **kwargs)
-        def isatty(self, *args, **kwargs): return self._file.isatty(*args, **kwargs)
-        def readable(self, *args, **kwargs): return self._file.readable(*args, **kwargs)
-        def readline(self, *args, **kwargs): return self._file.readline(*args, **kwargs)
-        def readlines(self, *args, **kwargs): return self._file.readlines(*args, **kwargs)
+
+        def isatty(self, *args, **
+                   kwargs): return self._file.isatty(*args, **kwargs)
+
+        def readable(self, *args, **
+                     kwargs): return self._file.readable(*args, **kwargs)
+
+        def readline(self, *args, **
+                     kwargs): return self._file.readline(*args, **kwargs)
+        def readlines(
+            self, *args, **kwargs): return self._file.readlines(*args, **kwargs)
+
         def seek(self, *args, **kwargs): return self._file.seek(*args, **kwargs)
         def tell(self, *args, **kwargs): return self._file.tell(*args, **kwargs)
-        def truncate(self, *args, **kwargs): return self._file.truncate(*args, **kwargs)
-        def writable(self, *args, **kwargs): return self._file.writable(*args, **kwargs)
+
+        def truncate(self, *args, **
+                     kwargs): return self._file.truncate(*args, **kwargs)
+        def writable(self, *args, **
+                     kwargs): return self._file.writable(*args, **kwargs)
 
         def read(self, *args, **kwargs): return self._file.read(*args, **kwargs)
-        def readall(self, *args, **kwargs): return self._file.readall(*args, **kwargs)
-        def readinto(self, *args, **kwargs): return self._file.readinto(*args, **kwargs)
+
+        def readall(self, *args, **
+                    kwargs): return self._file.readall(*args, **kwargs)
+        def readinto(self, *args, **
+                     kwargs): return self._file.readinto(*args, **kwargs)
+
         def write(self, *args, **kwargs): return self._file.write(*args, **kwargs)
 
 
@@ -134,24 +152,17 @@ class TemporaryNamedPipe:
 def stdout_redirected(to=os.devnull, stdout=None):
     """
     Context manager.
-
     Redirects a file object or file descriptor to a new file descriptor.
-
     Example:
     with open('my-stdout.txt', 'w') as f:
         with stdout_redirected(f):
             print('Writing to my-stdout.txt')
-
     Motivation: In pure-Python, you can redirect all print() statements like this:
-
         sys.stdout = open('myfile.txt')
-
         ...but that doesn't redirect any compiled printf() (or std::cout) output
         from C/C++ extension modules.
-
     This context manager uses a superior approach, based on low-level Unix file
     descriptors, which redirects both Python AND C/C++ output.
-
     Lifted from the following link (with minor edits):
     https://stackoverflow.com/a/22434262/162094
     (MIT License)
